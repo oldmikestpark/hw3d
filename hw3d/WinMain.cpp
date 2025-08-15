6f710e25803f541c1dca3844478cee6d4dc77dbc
oldmikestpark <75532133@qq.com>
2025-08-15 20:06:58 +0800

    wheel delta test

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,4 +1,5 @@
 #include "Window.h"
+#include <sstream>
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -18,6 +19,31 @@ int CALLBACK WinMain(
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
+
+			static int i = 0;
+			while (!wnd.mouse.IsEmpty()) 
+			{
+				const auto e = wnd.mouse.Read();
+				switch (e.GetType()) 
+				{
+				case Mouse::Event::Type::WheelUp:
+					i++;
+					{
+						std::ostringstream oss;
+						oss << "Up: " << i;
+						wnd.SetWindowTitle(oss.str());
+					}
+					break;
+				case Mouse::Event::Type::WheelDown:
+					i--;
+					{
+						std::ostringstream oss;
+						oss << "Down: " << i;
+						wnd.SetWindowTitle(oss.str());
+					}
+					break;
+				}
+			}
 		}
 
 		if (gResult == -1)
