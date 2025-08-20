c77444c01d4b1663270b09a1d932dae8acac72e9
oldmikestpark <75532133@qq.com>
2025-08-20 21:53:05 +0800

    Revert "test mouse optional event sys"

--- hw3d/App.cpp
+++ hw3d/App.cpp
@@ -21,17 +21,6 @@ int App::Go()
 
 void App::DoFrame()
 {
-	while (const auto e = wnd.mouse.Read()) 
-	{
-		switch (e->GetType()) 
-		{
-		case Mouse::Event::Type::Move:
-			std::ostringstream oss;
-			oss << "Mouse moved to: (" << e->GetPosX() << ", " << e->GetPosY() << ")";
-			wnd.SetWindowTitle(oss.str());
-			break;
-		}
-	}
 	const float c = sin(timer.Peek()) / 2.0f + 0.5f;
 	wnd.Gfx().ClearBuffer(c, c, 1.0f);
 	wnd.Gfx().DrawTestTriangle();
