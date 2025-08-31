2a868e7bd4e75395050357998f86afc275546e55
oldmikestpark <75532133@qq.com>
2025-08-31 17:47:43 +0800

    Change Simulation Speed Menu

--- hw3d/App.cpp
+++ hw3d/App.cpp
@@ -105,7 +105,7 @@ void App::DoFrame()
 		{
 			ImGui::SliderFloat("Speed Factor", &speed_factor, 0.0f, 4.0f);
 			ImGui::Text("Application average % .3f ms / frame(% .1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			ImGui::Text("Status: %s", wnd.kbd.KeyIsPressed(VK_SPACE) ? "PAUSED" : "RUNNING");
+			ImGui::Text("Status: %s", wnd.kbd.KeyIsPressed(VK_SPACE) ? "PAUSED" : "RUNNING (hold spacebar to pause)");
 
 			ImGui::End();
 		}
