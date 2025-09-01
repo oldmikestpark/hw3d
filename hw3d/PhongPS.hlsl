4c7da17f7f75daa6824fc05b79a6eb4c65528c80
oldmikestpark <75532133@qq.com>
2025-09-02 00:34:26 +0800

    better, but position doesn't matter

--- hw3d/PhongPS.hlsl
+++ hw3d/PhongPS.hlsl
@@ -4,12 +4,12 @@ cbuffer LightCBuffer
 };
 
 static const float3 materialColor = { 0.7f, 0.7f, 0.9f };
-static const float3 ambient = { 0.15f, 0.15f, 0.15f };
+static const float3 ambient = { 0.05f, 0.05f, 0.05f };
 static const float3 diffuseColor = { 1.0f, 1.0f, 1.0f };
 static const float diffuseIntensity = 1.0f;
 static const float attConst = 1.0f;
-static const float attLin = 1.0f;
-static const float attQuau = 1.0f;
+static const float attLin = 0.045;
+static const float attQuau = 0.0075;
 
 float4 main(float3 worldPos : Position, float3 n : Normal) : SV_Target
 {
@@ -18,7 +18,7 @@ float4 main(float3 worldPos : Position, float3 n : Normal) : SV_Target
     const float distTol = length(vTol);
     const float3 dirTol = vTol / distTol;
     // diffuse attenuation
-    const float att = attConst + attLin * distTol + attQuau * (distTol * distTol);
+    const float att = 1 / (attConst + attLin * distTol + attQuau * (distTol * distTol));
     // diffuse intensity
     const float diffuse = diffuseColor * diffuseIntensity * att * max(0.0f, dot(n, dirTol));
     // final color
