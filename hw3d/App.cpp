186127fba2a9171bbdc127079c106e7a472a6b2c
oldmikestpark <75532133@qq.com>
2025-09-13 17:47:57 +0800

    bad camera rotation free mouse look

--- hw3d/App.cpp
+++ hw3d/App.cpp
@@ -30,20 +30,62 @@ void App::DoFrame()
 
 	light.Draw(wnd.Gfx());
 
-	if (const auto e = wnd.kbd.ReadKey()) 
+	while (const auto e = wnd.kbd.ReadKey()) 
 	{
-		if (e->IsPressed() && e->GetCode() == VK_INSERT) 
+		if (!e->IsPressed()) 
 		{
-			if (wnd.CursorEnabled()) 
+			continue;
+		}
+		switch (e->GetCode()) 
+		{
+		case VK_ESCAPE:
+			if (wnd.CursorEnabled())
 			{
 				wnd.DisableCursor();
 				wnd.mouse.EnableRaw();
 			}
-			else 
+			else
 			{
 				wnd.EnableCursor();
 				wnd.mouse.DisableRaw();
 			}
+			break;
+		}
+	}
+
+	if (!wnd.CursorEnabled()) 
+	{
+		if (wnd.kbd.KeyIsPressed('W')) 
+		{
+			cam.Translate({0.0f, 0.0f, dt});
+		}
+		if (wnd.kbd.KeyIsPressed('A')) 
+		{
+			cam.Translate({-dt, 0.0f, 0.0f});
+		}
+		if (wnd.kbd.KeyIsPressed('S')) 
+		{
+			cam.Translate({0.0f, 0.0f, -dt});
+		}
+		if (wnd.kbd.KeyIsPressed('D')) 
+		{
+			cam.Translate({dt, 0.0f, 0.0f});
+		}
+		if (wnd.kbd.KeyIsPressed('R')) 
+		{
+			cam.Translate({ 0.0f, dt, 0.0f });
+		}
+		if (wnd.kbd.KeyIsPressed('F')) 
+		{
+			cam.Translate({0.0f, -dt, 0.0f});
+		}
+	}
+
+	while (const auto delta = wnd.mouse.ReadRawDelta()) 
+	{
+		if (!wnd.CursorEnabled()) 
+		{
+			cam.Rotate(delta->x, delta->y);
 		}
 	}
 
