bd4df4a3ce79ace78ab215d4fcdde233a46ae9eb
oldmikestpark <75532133@qq.com>
2025-10-24 21:17:57 +0800

    flip y because textures

--- hw3d/PhongPSNormalMap.hlsl
+++ hw3d/PhongPSNormalMap.hlsl
@@ -28,7 +28,7 @@ float4 main(float3 worldPos : Position, float3 n : Normal, float2 tc : Texcoord)
     {
         const float3 normalSample = nmap.Sample(splr, tc).xyz;
         n.x = normalSample.x * 2.0f - 1.0f;
-        n.y = normalSample.y * 2.0f - 1.0f;
+        n.y = -normalSample.y * 2.0f + 1.0f;
         n.z = -normalSample.z;
     }
     const float3 vToL = lightPos - worldPos;
