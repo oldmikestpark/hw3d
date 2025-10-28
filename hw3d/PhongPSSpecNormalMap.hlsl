3960c2ee024f51ec355048e0fe815b2299acd71b
oldmikestpark <75532133@qq.com>
2025-10-28 16:46:58 +0800

    shader includes

--- hw3d/PhongPSSpecNormalMap.hlsl
+++ hw3d/PhongPSSpecNormalMap.hlsl
@@ -1,13 +1,5 @@
-cbuffer LightCBuf
-{
-    float3 viewLightPos;
-    float3 ambient;
-    float3 diffuseColor;
-    float diffuseIntensity;
-    float attConst;
-    float attLin;
-    float attQuad;
-};
+#include "PointLight.hlsli"
+#include "ShaderOps.hlsli"
 
 cbuffer ObjectCBuf
 {
@@ -24,54 +16,6 @@ Texture2D spec;
 Texture2D nmap;
 
 SamplerState splr;
-
-float3 MapNormal(
-    const in float3 tan,
-    const in float3 bitan,
-    const in float3 normal,
-    const in float2 tc,
-    uniform Texture2D nmap,
-    uniform SamplerState splr)
-{
-    const float3x3 tanToTarget = float3x3(tan, bitan, normal);
-
-    const float3 normalSample = nmap.Sample(splr, tc).xyz;
-    float3 tanNormal = normalSample * 2.0f - 1.0f;
-    
-    return normalize(mul(tanNormal, tanToTarget));
-}
-
-float Attenuate(uniform float attConst, uniform float attLin, uniform float attQuad, const in float distFragToL)
-{
-    return 1.0f / (attConst + attLin * distFragToL + attQuad * (distFragToL * distFragToL));
-}
-
-float3 Diffuse(
-    uniform float3 diffuseColor,
-    uniform float diffuseIntensity,
-    const in float att,
-    const in float3 viewDirFragToL,
-    const in float3 viewNormal
-)
-{
-    return diffuseColor * diffuseIntensity * att * max(0.0f, dot(viewDirFragToL, viewNormal));
-}
-
-float3 Speculate(
-    const in float3 specularColor,
-    uniform float specularIntensity,
-    const in float3 viewNormal,
-    const in float3 viewFragToL,
-    const in float3 viewPos,
-    const in float att,
-    const in float specularPower
-)
-{
-    const float3 w = viewNormal * dot(viewFragToL, viewNormal);
-    const float3 r = normalize(w * 2.0f - viewFragToL);
-    const float3 viewCamToFrag = normalize(viewPos);
-    return att * specularColor * specularIntensity * pow(max(0.0f, dot(-r, viewCamToFrag)), specularPower);
-}
 
 float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float3 viewTan : Tangent, float3 viewBitan : Bitangent, float2 tc : Texcoord) : SV_Target
 {
