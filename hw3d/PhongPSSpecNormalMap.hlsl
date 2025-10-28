9ff3f25aca68aee9ea712a3a0b5f0ded063851d9
oldmikestpark <75532133@qq.com>
2025-10-28 18:34:05 +0800

    refactoring obj space nmap / need to fix nmap texture

--- hw3d/PhongPSSpecNormalMap.hlsl
+++ hw3d/PhongPSSpecNormalMap.hlsl
@@ -1,5 +1,6 @@
-#include "PointLight.hlsli"
 #include "ShaderOps.hlsli"
+#include "LightVectorData.hlsli"
+#include "PointLight.hlsli"
 
 cbuffer ObjectCBuf
 {
@@ -17,22 +18,19 @@ Texture2D nmap;
 
 SamplerState splr;
 
-float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float3 viewTan : Tangent, float3 viewBitan : Bitangent, float2 tc : Texcoord) : SV_Target
+
+float4 main(float3 viewFragPos : Position, float3 viewNormal : Normal, float3 viewTan : Tangent, float3 viewBitan : Bitangent, float2 tc : Texcoord) : SV_Target
 {
+    // normalize the mesh normal
     viewNormal = normalize(viewNormal);
+    // replace normal with mapped if normal mapping enabled
     if (normalMapEnabled)
     {
         viewNormal = MapNormal(normalize(viewTan), normalize(viewBitan), viewNormal, tc, nmap, splr);
     }
 	// fragment to light vector data
-    const float3 viewFragToL = viewLightPos - viewPos;
-    const float distFragToL = length(viewFragToL);
-    const float3 viewDirFragToL = viewFragToL / distFragToL;
-	
-	// reflected light vector
-    const float3 w = viewNormal * dot(viewFragToL, viewNormal);
-    const float3 r = w * 2.0f - viewFragToL;
-	// calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
+    const LightVectorData lv = CalculateLightVectorData(viewLightPos, viewFragPos);
+    // specular parameter determination (mapped or uniform)
     float3 specularReflectionColor;
     float specularPower = specularPowerConst;
     if (specularMapEnabled)
@@ -43,23 +41,21 @@ float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float3 viewTa
         {
             specularPower = pow(2.0f, specularSample.a * 13.0f);
         }
-
     }
     else
     {
         specularReflectionColor = specularColor;
     }
-    
-    // attenuation
-    const float att = Attenuate(attConst, attLin, attQuad, distFragToL);
-	// diffuse intensity
-    const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f, dot(viewDirFragToL, viewNormal));
-    
+
+	// attenuation
+    const float att = Attenuate(attConst, attLin, attQuad, lv.distToL);
+	// diffuse light
+    const float3 diffuse = Diffuse(diffuseColor, diffuseIntensity, att, lv.dirToL, viewNormal);
+    // specular reflected
     const float3 specularReflected = Speculate(
-        specularReflectionColor,
-        1.0f, viewNormal,
-        viewFragToL, viewPos, att, specularPower
+        specularReflectionColor, 1.0f, viewNormal,
+        lv.vToL, viewFragPos, att, specularPower
     );
-	// final color
+	// final color = attenuate diffuse & ambient by diffuse texture color and add specular reflected
     return float4(saturate((diffuse + ambient) * tex.Sample(splr, tc).rgb + specularReflected), 1.0f);
 }
