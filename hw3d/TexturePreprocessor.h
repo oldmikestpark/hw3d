bdba3060b28b031f60c56c2d53fca622e8b99e67
oldmikestpark <75532133@qq.com>
2025-10-29 17:28:47 +0800

    flip normal y components

---
+++ hw3d/TexturePreprocessor.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Surface.h"
+#include <string>
+#include <DirectXMath.h>
+
+class TexturePreprocessor 
+{
+public:
+	static void FlipYAllNormalMapsInObj(const std::string& objPath);
+private:
+	template<typename F>
+	static void TransformFile(const std::string& pathIn, const std::string& pathOut, F&& func);
+	static DirectX::XMVECTOR ColorToVector(Surface::Color c) noexcept;
+	static Surface::Color VectorToColor(DirectX::XMVECTOR n) noexcept;
+};
